bdc71cc465ba59244be2fcbf518eb0f850674008
unknown <berkanoztas11@gmail.com>
2025-11-21 21:48:55 +0100

    PWA: manifest, service worker, icônes, enregistrement

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "multijeuxgo-v1";
+const ASSETS = [
+  "/MultiJeuxGo/",
+  "/MultiJeuxGo/index.html",
+  "/MultiJeuxGo/style.css",
+  "/MultiJeuxGo/manifest.json",
+  "/MultiJeuxGo/icons/icon-192.png",
+  "/MultiJeuxGo/icons/icon-256.png",
+  "/MultiJeuxGo/icons/icon-384.png",
+  "/MultiJeuxGo/icons/icon-512.png",
+  // Ajoute ici toutes tes pages de jeux:
+  "/MultiJeuxGo/morpion.html",
+  "/MultiJeuxGo/quiz.html",
+  "/MultiJeuxGo/foot.html",
+  "/MultiJeuxGo/uno.html",
+  "/MultiJeuxGo/ludo.html",
+  "/MultiJeuxGo/bowling.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
