db6b521300f09432a848a6590def7ecadbb97344
unknown <berkanoztas11@gmail.com>
2025-11-24 17:06:16 +0100

    Publication automatique MultiJeuxGo

--- sw.js
+++ sw.js
@@ -33,3 +33,27 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("multi-jeuxgo-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js",
+        "/login.html",
+        "/register.html",
+        "/profil.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
